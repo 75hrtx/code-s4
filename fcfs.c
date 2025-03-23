205b0e45de2842d9cda868ab6ed7da7c86956d59
75hrtx <vishnu.navaneetham@outlook.com>
2025-03-23 22:54:23 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
-int main()  {
-    int n,bt[20],wt[20],tat[20],i,j;
-    float avwt = 0, avtat = 0;
- 
-    printf("Enter Total Number of processes (max 20) : ");
-    scanf("%d", &n);
-    
-    printf("\nEnter Process Burst Time\n");
-    for(i=0;i<n;i++) {
-        printf("P[%d] : ",i+1);
-        scanf("%d",&bt[i]);
-    }
-    wt[0]=0;
-    for(i=1;i<n;i++) {
-        wt[i]=0;
-        for(j=0;j<i;j++)
-            wt[i]+=bt[j];
-    }
-    
-    printf("Gantt chart: \n");
-    for(int k =0;k<n;k++) {
-        printf("P[%d] __ ",k+1);
-    }
-    printf("\n\n Process \tBurst Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
-    
-    for(i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];  // Turnaround Time = Burst Time + Waiting Time
-        avwt += wt[i];  // Sum of Waiting Times
-        avtat += tat[i]; // Sum of Turnaround Times
 
-        printf("P[%d] \t\t%d \t\t%d \t\t%d \t\t%d\n", i+1, bt[i], tat[i], wt[i], tat[i]);
-    }
-
-    
-    printf("\nAverage Waiting Time: %.2f", avwt/n);
-    printf("\nAverage Turnaround Time: %.2f\n", avtat/n);
-    
-    return 0;
+int main() {
+	int n,b[20],w[20],t[20]; //burst time, waiting time, turnaround time
+	float a=0,at=0; //avg waiting time, avg turnaround time
+	
+	printf("Enter the total no of processes: ");
+	scanf("%d",&n);
+	
+	printf("Enter the process burst time: ");
+	for(int i=0;i<n;i++) {
+		printf("p[%d]: ",i+1);
+		scanf("%d",&b[i]);
+	}
+	w[0]=0;
+	for(int i=1;i<n;i++) {
+		w[i]=0;
+		for(int j=0;j<i;j++)
+			w[i]+=b[j];
+	}
+	printf("Gantt Chart\n");
+	for(int k=0;k<n;k++) {
+		printf("P[%d]__",k+1);
+	}
+	
+	printf("\nProcess \tBurst time \tCompletion time \tWaiting timr\t \tTat\n");
+	
+	for(int i=0;i<n;i++) {
+		t[i] = b[i]+w[i]; //tat=bt+wt
+		a += w[i];	//awt= awt+wt
+		at += t[i];	//att= at+tt
+		printf("P[%d]\t\t %d \t%d \t%d \t%d\n",i+1,b[i],t[i],w[i],t[i]);
+	}	
+	printf("\nAvg Waiting time: %.2f",a/n);
+	printf("\nAvg Tat time: %.2f",at/n); 
 }
