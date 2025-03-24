ddf291c6c20d0dd8548105d6f74791526c6f9044
75hrtx <vishnu.navaneetham@outlook.com>
2025-03-24 11:43:42 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,65 +1,60 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    int pno;
-    int pri;
-    int bt;
-    int wt;
-} sp;
 
 int main() {
-    int i, j, n;
-    int tbm = 0, totwt = 0, tottat = 0;
-    sp *p, t;
-    
-    printf("\nEnter the Number of Processes: ");
-    scanf("%d", &n);
-
-    p = (sp*) malloc(n * sizeof(sp));
-
-    printf("\nEnter the Burst Time and Priority: \n");
-    for(i = 0; i < n; i++) {
-        printf("Process %d: ", i + 1);
-        scanf("%d %d", &p[i].bt, &p[i].pri);
-        p[i].pno = i + 1;
-        p[i].wt = 0;
-    }
-
-    for(i = 0; i < n - 1; i++) {
-        for(j = i + 1; j < n; j++) {
-            if(p[i].pri > p[j].pri) {
-                t = p[i];
-                p[i] = p[j];
-                p[j] = t;
-            }
-
-
-
-
-
-
-        }
-    }
-
-    printf("\nProcess\tBurst Time\tWaiting Time\tCompletion Time\tTurnaround Time\n");
-    for(i = 0; i < n; i++) {
-        totwt += p[i].wt = tbm;
-        tbm += p[i].bt;
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i].pno, p[i].bt, p[i].wt, p[i].wt + p[i].bt, p[i].wt + p[i].bt);
-    }
-
-    tottat = tbm + totwt;
-
-    printf("\nGantt chart:\n");
-    for(i = 0; i < n; i++) {
-        printf("p[%d]_", p[i].pno);
-    }
-
-    printf("\nAverage Waiting Time: %.2f", (float)totwt / n);
-    printf("\nAverage Turnaround Time: %.2f\n", (float)tottat / n);
-    free(p);
-
-    return 0;
+	int n,b[20],w[20],t[20],pr[20],pi[20]; //burst time, waiting time, turnaround time
+	float a=0,at=0; //avg waiting time, avg turnaround time
+	int temp;
+	
+	printf("Enter the total no of processes: ");
+	scanf("%d",&n);
+	
+	printf("Enter the process burst time: ");
+	for(int i=0;i<n;i++) {
+		printf("p[%d]: ",i+1);
+		scanf("%d",&b[i]);
+		printf("p[%d] Priority: ",i+1);
+		scanf("%d",&pr[i]);
+		pi[i]=i+1;
+	}
+	
+	//sort based on priority
+	for(int i=0;i<n-1;i++) {
+		for(int j=i+1;j<n;j++) {
+			if(pr[i] > pr[j]) {
+				temp=b[i];
+				b[i]=b[j];
+				b[j]=temp;
+				
+				temp=pr[i];
+				pr[i]=pr[j];
+				pr[j]=temp;
+				
+				temp=pi[i];
+				pi[i]=pi[j];
+				pi[j]=temp;
+			}
+		}
+	}
+	
+	w[0]=0;
+	for(int i=1;i<n;i++) {
+		w[i]=0;
+		for(int j=0;j<i;j++)
+			w[i]+=b[j];
+	}
+	printf("Gantt Chart\n");
+	for(int k=0;k<n;k++) {
+		printf("P[%d]__",pi[k]);
+	}
+	
+	printf("\nProcess \tBurst time \tCompletion time \tWaiting timr\t \tTat\n");
+	
+	for(int i=0;i<n;i++) {
+		t[i] = b[i]+w[i]; //tat=bt+wt
+		a += w[i];	//awt= awt+wt
+		at += t[i];	//att= at+tt
+		printf("P[%d]\t\t %d \t%d \t%d \t%d\n",i+1,b[i],t[i],w[i],t[i]);
+	}	
+	printf("\nAvg Waiting time: %.2f",a/n);
+	printf("\nAvg Tat time: %.2f",at/n); 
 }
-
