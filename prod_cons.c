39482329299c45ca9a66c8b15fd2e9e5fd481c35
75hrtx <vishnu.navaneetham@outlook.com>
2025-03-24 11:49:01 +0530

    Update prod_cons.c

--- prod_cons.c
+++ prod_cons.c
@@ -6,58 +6,43 @@
 
 sem_t empty, full;
 pthread_mutex_t mutex;
-int buffer[5];
-int count = 0;
+int buffer[5], count = 0;
 
 void* producer(void* arg) {
-    long int num = (long int) arg;
     sem_wait(&empty);
     pthread_mutex_lock(&mutex);
-    buffer[count] = rand() % 10;
-    printf("\nProducer %ld Produced: %d", num + 1, buffer[count]);
-    count++;
-    sleep(1);
+    buffer[count++] = rand() % 10;
+    printf("\nProducer %ld Produced: %d", (long)arg + 1, buffer[count - 1]);
     pthread_mutex_unlock(&mutex);
     sem_post(&full);
-    pthread_exit(NULL);
+    return NULL;
 }
 
 void* consumer(void* arg) {
-    long int num = (long int) arg;
     sem_wait(&full);
     pthread_mutex_lock(&mutex);
-    count--;
-    printf("\nConsumer %ld Consumed: %d", num + 1, buffer[count]);
-    sleep(1);
+    printf("\nConsumer %ld Consumed: %d", (long)arg + 1, buffer[--count]);
     pthread_mutex_unlock(&mutex);
     sem_post(&empty);
-    pthread_exit(NULL);
+    return NULL;
 }
 
 int main() {
     int np, nc;
-    pthread_t p[10], c[10];
-    unsigned long int i, j, k, l;
-
-    printf("Enter number of producers and number of consumers: ");
+    printf("Enter number of producers and consumers: ");
     scanf("%d %d", &np, &nc);
 
     sem_init(&empty, 0, 5);
     sem_init(&full, 0, 0);
-    pthread_mutex_init(&mutex,NULL);
+    pthread_mutex_init(&mutex, NULL);
 
-    for(i=0;i<np;i++)
-        pthread_create(&p[i],NULL,producer,(void*) i);
+    pthread_t p[np], c[nc];
 
-    for(j=0;j<nc;j++)
-        pthread_create(&c[j],NULL,consumer,(void*) j);
+    for (int i = 0; i < np; i++) pthread_create(&p[i], NULL, producer, (void*)(long)i);
+    for (int i = 0; i < nc; i++) pthread_create(&c[i], NULL, consumer, (void*)(long)i);
 
-    for(k=0;k<np;k++)
-        pthread_join(p[k],NULL);
-
-    for(l=0;l<nc;l++)
-        pthread_join(c[l],NULL);
+    for (int i = 0; i < np; i++) pthread_join(p[i], NULL);
+    for (int i = 0; i < nc; i++) pthread_join(c[i], NULL);
 
     return 0;
 }
-
